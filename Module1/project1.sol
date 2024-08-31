522d6c27029566bbf5a6b8cac79abef2b7225f9a
Amitsingla1234 <amitsingla816@gmail.com>
2024-08-31 22:04:14 +0530

    Update project1.sol

--- Module1/project1.sol
+++ Module1/project1.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
 
+/*
+REQUIREMENTS
+Contract successfully uses require()
+Contract successfully uses assert()
+Contract successfully uses revert() statements
+*/
+pragma solidity ^0.8.20;
+
+contract UtilityBilling {
+    uint public electricityUnits;
+    bool public billPaidStatus;
+
+// Function to set the electricity bill with a require statement
+    function updateElectricityBill(uint _units, bool _isBillPaid) public {
+        require(_units > 100, "Units must be greater than 100");
+
+// Revert function is used 
+        if (!_isBillPaid) {
+            revert("Please pay the electricity bill");
+        }
+
+// Assert function is used
+        assert(_isBillPaid == true);
+
+        electricityUnits = _units;
+        billPaidStatus = _isBillPaid;
+    }
+}
